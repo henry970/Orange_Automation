3f421f37850d2f5ff7fbeec606ace84d2d32a1c8
henry970 <henryokolie615@gmail.com>
2024-05-26 19:18:25 +0100

    update admin and PIM page

--- Orange_automation_test/test_Orange.py
+++ Orange_automation_test/test_Orange.py
@@ -1,33 +1,36 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 from Orange_automation_test.Admin_page.admin_page import AdminPage
 from Orange_automation_test.Orange_actionPage.ActionPage import LoginPage
 from Orange_automation_test.PIM_page.PIM_page_test import PIMPage
 
 
-# @pytest.fixture(scope="module")
-# def driver_setup():
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless")
-#     chrome_options.add_argument("--no-sandbox")
-#     chrome_options.add_argument("--disable-dev-shm-usage")
-#     chrome_options.add_argument("--disable-gpu")
-#     chrome_options.add_argument("--window-size=1920x1080")
-#
-#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#     driver.implicitly_wait(20)
-#     yield driver
-#     driver.quit()
-
-
 @pytest.fixture(scope="module")
 def driver_setup():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.implicitly_wait(20)
-    driver.maximize_window()
     yield driver
     driver.quit()
+
+#
+# @pytest.fixture(scope="module")
+# def driver_setup():
+#     driver = webdriver.Chrome()
+#     driver.implicitly_wait(20)
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
 
 
 @pytest.fixture(scope="module")
